5af708d15b0dcd8dae17d3057be0dce7a06b9b4e
Bojan Adzic <39236370+Bojan2111@users.noreply.github.com>
2021-01-10 11:17:57 +0100

    Updated with solution code

--- server.js
+++ server.js
@@ -30,3 +30,24 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// Solution by Coder::Owl aka Bojan Adzic
+
+app.get('/api/timestamp/:dateString?', (req, res) => {
+	const dateString = req.params.dateString;
+	let date;
+	if (!dateString) {
+		date = new Date();
+	} else {
+		if (!isNaN(dateString)) {
+			date = new Date(parseInt(dateString));
+		} else {
+			date = new Date(dateString);
+		}
+	}
+	if (date.toString() === 'Invalid Date') {
+		res.json({ error: date.toString() });
+	} else {
+		res.json({ unix: date.getTime(), utc: date.toUTCString() });
+	}
+});
